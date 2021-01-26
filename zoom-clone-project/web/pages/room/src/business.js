668a9d7796d8fd39d02b9af899ab5b9e31234977
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-01-26 15:11:45 -0400

    add video to client peer

--- zoom-clone-project/web/pages/room/src/business.js
+++ zoom-clone-project/web/pages/room/src/business.js
@@ -1,17 +1,16 @@
 class Business {
-  constructor({ room, media, view, socketBuilder }) {
+  constructor({ room, media, view, socketBuilder, peerBuilder }) {
     this.room = room;
     this.media = media;
     this.view = view;
 
-    this.socketBuilder = socketBuilder
-      .setOnUserConnected(this.onUserConnected())
-      .setOnUserDisconnected(this.onUserDisconnected())
-      .build();
-
-    this.socketBuilder.emit("join-room", this.room, "test01");
+    this.socketBuilder = socketBuilder;
+    this.peerBuilder = peerBuilder;
 
     this.currentStream = {};
+    this.currentPeer = {};
+
+    this.socket = {};
   }
 
   static initialize(deps) {
@@ -23,6 +22,20 @@ class Business {
   async _init() {
     //                                    habilitando som
     this.currentStream = await this.media.getCamera(true);
+
+    this.socket = this.socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build();
+
+    // sem await gera error
+    this.currentPeer = await this.peerBuilder
+      .setOnError(this.onPeerError())
+      .setOnConnectionOpened(this.onPeerConnectionOpened())
+      .setOnCallReceived(this.onPeerCallReceived())
+      .setOnPeerStreamReceived(this.onPeerStreamReceived())
+      .build();
+
     console.log("init", this.currentStream);
     this.addVideoStream("test01");
   }
@@ -34,13 +47,43 @@ class Business {
 
   onUserConnected = function () {
     return (userId) => {
-      console.log("user connected", userId);
+      console.log("user connected!", userId);
+
+      this.currentPeer.call(userId, this.currentStream);
     };
   };
 
   onUserDisconnected = function () {
     return (userId) => {
-      console.log("user disconnected", userId);
+      console.log("user disconnected!", userId);
+    };
+  };
+
+  onPeerError = function () {
+    return (error) => {
+      console.log("error on peer!", error);
+    };
+  };
+
+  onPeerConnectionOpened = function () {
+    return (peer) => {
+      const id = peer.id;
+      console.log("peer!", peer);
+      this.socket.emit("join-room", this.room, id);
+    };
+  };
+
+  onPeerCallReceived = function () {
+    return (call) => {
+      console.log("answering call!", call);
+      call.answer(this.currentStream);
+    };
+  };
+
+  onPeerStreamReceived = function () {
+    return (call, stream) => {
+      const callerId = call.peer;
+      this.addVideoStream(callerId, stream);
     };
   };
 }
--- zoom-clone-project/web/pages/room/src/util/peer.js
+++ zoom-clone-project/web/pages/room/src/util/peer.js
@@ -1,3 +1,58 @@
 class PeerBuilder {
-  constructor({}) {}
+  constructor({ peerConfig }) {
+    this.peerConfig = peerConfig;
+
+    const defaultFunctionValue = () => {};
+
+    this.onError = defaultFunctionValue;
+    this.onCallReceived = defaultFunctionValue;
+    this.onConnectionOpened = defaultFunctionValue;
+    this.onPeerStreamReceived = defaultFunctionValue;
+  }
+
+  setOnError(fn) {
+    this.onError = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  setOnCallReceived(fn) {
+    this.onCallReceived = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  setOnConnectionOpened(fn) {
+    this.onConnectionOpened = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  setOnPeerStreamReceived(fn) {
+    this.onPeerStreamReceived = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  _prepareCallEvent(call) {
+    call.on("stream", (stream) => {
+      this.onPeerStreamReceived(call, stream);
+    });
+
+    this.onCallReceived(call);
+  }
+
+  build() {
+    const peer = new Peer(...this.peerConfig);
+
+    peer.on("error", this.onError);
+    peer.on("call", this._prepareCallEvent.bind(this));
+
+    return new Promise((resolve) => {
+      peer.on("open", (id) => {
+        this.onConnectionOpened(peer);
+        return resolve(peer);
+      });
+    });
+  }
 }
