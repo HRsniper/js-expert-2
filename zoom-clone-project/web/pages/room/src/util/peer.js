ad4ab569d078170fc5b4556997c8c3613d97677b
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-01-27 16:35:06 -0400

    mostranod video tbm para o inicializador da sala

--- zoom-clone-project/web/pages/room/src/util/peer.js
+++ zoom-clone-project/web/pages/room/src/util/peer.js
@@ -8,10 +8,24 @@ class PeerBuilder {
     this.onCallReceived = defaultFunctionValue;
     this.onConnectionOpened = defaultFunctionValue;
     this.onPeerStreamReceived = defaultFunctionValue;
+    this.onCallError = defaultFunctionValue;
+    this.onCallClose = defaultFunctionValue;
   }
 
   setOnError(fn) {
     this.onError = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  setOnCallError(fn) {
+    this.onCallError = fn;
+    // retorna a instância da classe para chamar as outras funções da classe
+    return this;
+  }
+
+  setOnCallClose(fn) {
+    this.onCallClose = fn;
     // retorna a instância da classe para chamar as outras funções da classe
     return this;
   }
@@ -39,11 +53,40 @@ class PeerBuilder {
       this.onPeerStreamReceived(call, stream);
     });
 
+    call.on("error", (error) => {
+      this.onCallError(call, error);
+    });
+
+    call.on("close", (_) => {
+      this.onCallClose(call);
+    });
+
     this.onCallReceived(call);
   }
 
+  // adicionar o comportamento dos eventos de CALL também para quem ligar
+  _preparePeerInstanceFunction(peerModule) {
+    class PeerCustomModule extends peerModule {}
+
+    const peerCall = PeerCustomModule.prototype.call;
+    const context = this;
+
+    PeerCustomModule.prototype.call = function (id, stream) {
+      const call = peerCall.apply(this, [id, stream]);
+
+      // intercepta a CALL e adiciona todos eventos de chamada para quem liga também
+      context._prepareCallEvent(call);
+
+      return call;
+    };
+
+    return PeerCustomModule;
+  }
+
   build() {
-    const peer = new Peer(...this.peerConfig);
+    // const peer = new Peer(...this.peerConfig);
+    const PeerCustomInstance = this._preparePeerInstanceFunction(Peer);
+    const peer = new PeerCustomInstance(...this.peerConfig);
 
     peer.on("error", this.onError);
     peer.on("call", this._prepareCallEvent.bind(this));
